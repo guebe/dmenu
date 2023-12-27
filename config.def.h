10f048fd3073afe907208bba1795e41f1f860f91
gue <gue@debian.home>
2023-12-27 20:10:49 +0100

    changed colors to light background

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#000000", "#F6F5F4" },
+	[SchemeSel] = { "#F6F5F4", "#3584E4" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
